86650f7a07a9281d99c6aef611d8aecc31f40550
alko knijff <alkoknijff@gmail.com>
2026-02-22 15:32:51 +0100

    Add levels

--- scripts/World.cs
+++ scripts/World.cs
@@ -1,11 +1,15 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.Marshalling;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public partial class World : Node2D
 {
 	private TextureButton _playButton;
+	private Label _levelCounter;
 	private Label _turnCounter;
 	private Panel _messagePanel;
 	private GridOverlay _gridOverlay;
@@ -18,17 +22,22 @@ public partial class World : Node2D
 	private int _enemyUnitsCount = 0;
 	
   private bool _playing = false;
-	private int _turn = 0;
+	private int _level = 1;
+  private int _turn = 0;
   private double _turnCooldown = 0.25f;
   private double _turnStartCooldown = 0.25f;
   bool _gameEnd = false;
   private double _gameEndCooldown = 4.0f;
   private double _gameEndStartCooldown = 4.0f;
 
+	private Dictionary _unitsData;
+	private Dictionary _levelsData;
+
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
 	{
 		_playButton = GetNode<TextureButton>("CanvasLayer/BottomUi/PlayButton");
+		_levelCounter = GetNode<Label>("CanvasLayer/BottomUi/LevelCounter/Counter");
 		_turnCounter = GetNode<Label>("CanvasLayer/BottomUi/TurnCounter/Counter");
 		_messagePanel = GetNode<Panel>("CanvasLayer/MessagePanel");
 		_gridOverlay = GetNode<GridOverlay>("GridOverlay");
@@ -38,6 +47,14 @@ public partial class World : Node2D
 		_playButton.Pressed += OnPlayButtonPressed;
 		_units = new List<Unit>();
     _globalSignals.UnitDied += OnUnitDied;
+
+    string unitsJson = FileAccess.Open("res://scripts/units/units.json", FileAccess.ModeFlags.Read).GetAsText();
+    Variant parsed = Json.ParseString(unitsJson);
+		_unitsData = (Dictionary)parsed;
+
+    string levelsJson = FileAccess.Open("res://scripts/levels.json", FileAccess.ModeFlags.Read).GetAsText();
+    parsed = Json.ParseString(levelsJson);
+    _levelsData = (Dictionary)parsed;
   }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -101,16 +118,28 @@ public partial class World : Node2D
 	private Unit[,] loadLevel()
 	{
 		Unit[,] unitGrid = new Unit[GlobalConstants.GridSize.X, GlobalConstants.GridSize.Y];
+		Dictionary levelData = (Dictionary)_levelsData["level_" + _level.ToString()];
+    Godot.Collections.Array enemyUnitsData = (Godot.Collections.Array)levelData["units"];
+    foreach (Dictionary unitData in enemyUnitsData)
+    {
+      int x = (int)unitData["x"];
+      int y = (int)unitData["y"];
+      string name = (string)unitData["name"];
 
-		// Place the unit in the new cell
-		Vector2I relCel = new Vector2I(4, 4);
-		PackedScene scene = GD.Load<PackedScene>("res://scenes/units/turret.tscn");
-		Node instance = scene.Instantiate();
-		Unit unit = instance as Unit;
-		unit.GlobalPosition = _gridOverlay.RelCellToGlobalPosition(relCel, false);
-		unit.side = false;
-    _unitsNode.AddChild(instance);
-		unitGrid[relCel.X, relCel.Y] = unit;
+      Dictionary unitTemplate = (Dictionary)_unitsData[name];
+
+      // Parse template data
+      string scenePath = (string)unitTemplate["scene"];
+
+      // Place the unit in the new cell
+      PackedScene scene = GD.Load<PackedScene>(scenePath);
+      Node instance = scene.Instantiate();
+      Unit unit = instance as Unit;
+      unit.GlobalPosition = _gridOverlay.RelCellToGlobalPosition(new Vector2I(x, y), false);
+      unit.side = false;
+      _unitsNode.AddChild(instance);
+      unitGrid[x, y] = unit;
+    }
 
 		return unitGrid;
   }
@@ -131,6 +160,7 @@ public partial class World : Node2D
 		_gameEnd = false;
 		_gridOverlay.LoadUnits();
 		_playButton.Disabled = false;
+    _levelCounter.Text = _level.ToString();
   }
 
 	private void win()
@@ -138,6 +168,7 @@ public partial class World : Node2D
 		_playing = false;
 		_gameEnd = true;
 		_messagePanel.GetNode<RichTextLabel>("Message").Text = "You Win!";
+    _level += 1;
   }
 
   private void lose()
